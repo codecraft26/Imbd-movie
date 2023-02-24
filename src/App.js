4d31c6ccc0d902f8dfa109341119dc0bf0f6e92e
codecraft26 <gaman0221@gmail.com>
2023-02-24 21:05:13 +0530

    setting routes for papular movie + toprated + upcoming

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import Home from "./pages/home/Home"
+import MovieList from './components/movieList/MovieList';
+import Movie from './pages/movieDetail/Movie';
 
 
 function App() {
@@ -16,7 +18,11 @@ function App() {
           {/* creating routes for index element that prefer to home page  */}
         
           <Route index element={<Home />}></Route>
+          <Route path="movies/:type" element={<MovieList />}></Route>
+
+          <Route path="movie/:id" element={<Movie />}></Route>
         </Routes>
+        {/* <Route path="movies/:type" element={<MovieList />}></Route> */}
 
 
 
